5feff234683ce5c8df3d6969476281f56ca032dd
Alexander Rössler <mail@roessler.systems>
2019-07-04 15:57:39 +0200

    fix test

--- {{cookiecutter.project_slug}}/{{cookiecutter.module_name}}/tests/test_calculator.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.module_name}}/tests/test_calculator.py
@@ -9,8 +9,8 @@ def calc():
     return Calculator()
 
 
-@pytest.mark.parametrize("a,b,expected", [(1, 1, 2), -1, 2, 1])
+@pytest.mark.parametrize("a,b,expected", [(1, 1, 2), (-1, 2, 1)])
 def test_sum_is_calculated_correctly(calc, a, b, expected):
     calc.a = a
     calc.b = b
-    assert calc.out == expected
+    assert calc.sum == expected
